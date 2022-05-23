839c3b68aeacba36ddd7c41b04cd9ec300f1bb69
Matej Lazar <matejonnet@gmail.com>
2022-05-23 14:08:45 +0200

    Update copyright headers.

--- src/test/java/org/jboss/pnc/bifrost/source/RemoteConnectionTest.java
+++ src/test/java/org/jboss/pnc/bifrost/source/RemoteConnectionTest.java
@@ -1,6 +1,22 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2020-2022 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.pnc.bifrost.source;
 
-import io.quarkus.arc.DefaultBean;
 import io.quarkus.test.junit.QuarkusTest;
 import org.apache.http.HttpEntity;
 import org.apache.http.entity.BasicHttpEntity;
@@ -8,7 +24,6 @@ import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.jboss.pnc.api.bifrost.dto.Line;
 import org.jboss.pnc.api.bifrost.enums.Direction;
-import org.jboss.pnc.bifrost.common.MainBean;
 import org.jboss.pnc.bifrost.endpoint.provider.DataProvider;
 import org.jboss.pnc.bifrost.test.DebugTest;
 import org.junit.jupiter.api.Assertions;
@@ -16,7 +31,6 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.Collections;
