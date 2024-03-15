e23199a39db7ea3759f0e15779db5efe4157ddc2
Dustin Kut Moy Cheung <dcheung@redhat.com>
2024-03-15 16:48:33 -0400

    Fix LogEntryDbRepository::get method
    The LogEntryDbRepository::get method is used to:
    1. check if there is an existing logentry matching the process context,
       process context variant, and other values other than id
    2. if there's a match, return that match
    3. if there isn't, store that logentry
    
    Step 3 fails consistenly in Bifrost with this error:
    ```
    [2024-03-15T20:19:27.837Z] DEBUG [org.jboss.pnc.bifrost.source.db.LogEntryDbRepository] Persisting LogEntry: LogEntry(id=0, processContext=556571267898798080, processContextVariant=0, requestContext=, temporary=false, buildId=null)
    [2024-03-15T20:19:27.839Z] ERROR [org.hibernate.engine.jdbc.batch.internal.BatchingBatch] HHH000315: Exception executing batch [java.sql.BatchUpdateException: Batch entry 0 insert into LogEntry (buildId, processContext, processContextVariant, requestContext, temporary, id) values (NULL, 556571267898798080, '0', '', 'FALSE', 0) was aborted: ERROR: duplicate key value violates unique constraint "logentry_pkey"
      Detail: Key (id)=(0) already exists.  Call getNextException to see other errors in the batch.], SQL: insert into LogEntry (buildId, processContext, processContextVariant, requestContext, temporary, id) values (?, ?, ?, ?, ?, ?)
    [2024-03-15T20:19:27.839Z] WARN [org.hibernate.engine.jdbc.spi.SqlExceptionHelper] SQL Error: 0, SQLState: 23505
    [2024-03-15T20:19:27.839Z] ERROR [org.hibernate.engine.jdbc.spi.SqlExceptionHelper] Batch entry 0 insert into LogEntry (buildId, processContext, processContextVariant, requestContext, temporary, id) values (NULL, 556571267898798080, '0', '', 'FALSE', 0) was aborted: ERROR: duplicate key value violates unique constraint "logentry_pkey"
      Detail: Key (id)=(0) already exists.  Call getNextException to see other errors in the batch.
    [2024-03-15T20:19:27.839Z] ERROR [org.hibernate.engine.jdbc.spi.SqlExceptionHelper] ERROR: duplicate key value violates unique constraint "logentry_pkey"
      Detail: Key (id)=(0) already exists.
    ```
    
    This is because we tend to use that method by building a `LogEntry`
    object with no id set. Since we didn't configure `LogEntry` to set an id
    if nothing is set, the default of the id is 0, and the insertion in step
    3 will fail with duplicate key.
    
    This commit fixes it by:
    - setting a unique id on step 3.
    
    Tests are written to make sure that error doesn't happen again.

---
+++ src/test/java/org/jboss/pnc/bifrost/source/db/LogEntryTest.java
@@ -0,0 +1,31 @@
+package org.jboss.pnc.bifrost.source.db;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.jboss.pnc.common.Random;
+import org.jboss.pnc.common.concurrent.Sequence;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.transaction.Transactional;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+class LogEntryTest {
+
+    @Test
+    @Transactional
+    void findExisting() {
+        long processContext = Random.randInt(0, 10000);
+        String processContextVariant = "0";
+        LogEntry logEntry = new LogEntry(Sequence.nextId(), processContext, processContextVariant, null, false, null);
+        logEntry.persist();
+
+        LogEntry toFind = new LogEntry(0L, processContext, processContextVariant, null, false, null);
+
+        Optional<LogEntry> existing = LogEntry.findExisting(toFind);
+        Assertions.assertTrue(existing.isPresent());
+    }
+}
