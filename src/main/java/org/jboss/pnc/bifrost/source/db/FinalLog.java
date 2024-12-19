c0b4b5d5d757e7f334b9f3b17a49b5bb9bd672a1
Dustin Kut Moy Cheung <dcheung@redhat.com>
2024-12-19 10:32:24 -0500

    Re-order the params in assertEquals

--- src/main/java/org/jboss/pnc/bifrost/source/db/FinalLog.java
+++ src/main/java/org/jboss/pnc/bifrost/source/db/FinalLog.java
@@ -27,7 +27,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import java.io.IOException;
